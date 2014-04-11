36e1bbdf0ab773a0940c10d89821e3d25fc1a7cd
Josh Cheek <josh.cheek@gmail.com>
2014-04-11 11:50:01 -0600

    Rename generate_requests -> GenerateRequests
    Also print and exit when the url is wrong

--- GenerateRequests.java
+++ GenerateRequests.java
@@ -5,11 +5,11 @@ import java.util.concurrent.ExecutorService;
 import java.io.IOException;
 import java.util.concurrent.Semaphore;
 
-public class generate_requests implements Runnable {
+public class GenerateRequests implements Runnable {
   public static void main(String[] args) throws Exception {
     String            rawUrl     = (args.length > 0 ? args[0] : "http://localhost:4567/ping");
     URL               url        = new URL(rawUrl);
-    generate_requests requester  = new generate_requests(url);
+    GenerateRequests  requester  = new GenerateRequests(url);
 
     long              startTime  = System.nanoTime();
     ExecutorService   threadPool = Executors.newFixedThreadPool(20);
@@ -24,7 +24,7 @@ public class generate_requests implements Runnable {
   private Semaphore countSemaphore;
 
 
-  public generate_requests(URL url) {
+  public GenerateRequests(URL url) {
     this.count          = 0;
     this.countSemaphore = new Semaphore(1);
     this.startTime      = System.nanoTime();
@@ -43,7 +43,11 @@ public class generate_requests implements Runnable {
       ++count;
       System.out.println("Request " + count + " responded after " + seconds);
       countSemaphore.release();
-    } catch (IOException          e) { /* no op */ }
-      catch (InterruptedException e) { countSemaphore.release(); }
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+      System.exit(1);
+    } catch (InterruptedException e) {
+      countSemaphore.release();
+    }
   }
 }
