159f5ae7c4f5fd95c108ed458c2bee80d502d39d
Josh Cheek <josh.cheek@gmail.com>
2014-04-11 10:52:55 -0600

    generate_requests uses a threadpool

--- generate_requests.java
+++ generate_requests.java
@@ -3,37 +3,59 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.io.IOException;
+// import java.util.concurrent.Semaphore;
+
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class generate_requests {
+public class generate_requests implements Runnable {
   public static void main(String[] args) throws Exception {
-    String            rawUrl    = (args.length > 0 ? args[0] : "http://localhost:4567/ping");
-    URL               url       = new URL(rawUrl);
-    generate_requests requester = new generate_requests(url);
+    String            rawUrl     = (args.length > 0 ? args[0] : "http://localhost:4567/ping");
+    URL               url        = new URL(rawUrl);
+    generate_requests requester  = new generate_requests(url);
 
-    long startTime = System.nanoTime();
-    for(int i = 0; i < 10; ++i) {
-      int response = requester.makeRequest();
-      if(response != 200) {
-        System.err.println("Expected 200 response, got " + response);
+    long              startTime  = System.nanoTime();
+    ExecutorService   threadPool = Executors.newFixedThreadPool(20);
+
+    for(int i = 0; i < 20; ++i) { threadPool.execute(requester); }
+    threadPool.shutdown();
+  }
+
+  private URL       url;
+  private int       count;
+  private long      startTime;
+  // private Semaphore countSemaphore;
+
+
+  public generate_requests(URL url) {
+    this.count          = 0;
+    // this.countSemaphore = new Semaphore(-1);
+    this.startTime      = System.nanoTime();
+    this.url            = url;
+  }
+
+  public void run() {
+    try {
+      int responseCode = ((HttpURLConnection)url.openConnection()).getResponseCode();
+      if(responseCode != 200) {
+        System.err.println("Expected 200 response, got " + responseCode);
         System.exit(1);
       }
-      long endTime = System.nanoTime();
-      float seconds = (endTime - startTime) / 1000000000L;
-      System.out.println("Request " + i + "responded after " + seconds + " seconds");
+      float seconds = (System.nanoTime() - startTime) / 1000000000L;
+      System.out.println("Request " + count + " responded after " + seconds);
+      // try {
+      //   countSemaphore.acquire();
+         ++count;
+      // } catch (InterruptedException e) {
+      //   // no op
+      // } finally {
+      //   countSemaphore.release();
+      // }
+    } catch (IOException e) {
+      // no op
     }
-  }
-
-  URL url;
-  public generate_requests(URL url) {
-    this.url = url;
-  }
-
-  public int makeRequest() throws Exception {
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    // con.setRequestMethod("GET");
-    // con.setRequestProperty("User-Agent", "Mozilla/5.0");
-    return con.getResponseCode();
   }
 }
